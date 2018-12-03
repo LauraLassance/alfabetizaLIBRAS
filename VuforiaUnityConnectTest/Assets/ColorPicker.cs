b9dd8ed13ad4723e63d481769e1551289ac4f0d7
Amauri Shimabukuro <amaurishim@gmail.com>
2018-12-03 16:18:50 -0200

    adicionando botao de troca de cor

---
+++ VuforiaUnityConnectTest/Assets/ColorPicker.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorPicker : MonoBehaviour {
+
+    public Material[] BodyColorMat;
+    Material CurrMat;
+    Renderer renderer;
+
+	// Use this for initialization
+	void Start () {
+
+        renderer = this.GetComponent<Renderer>();
+
+	}
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    //render blue color
+    public void BlueColor()
+    {
+        renderer.material = BodyColorMat[0];
+        CurrMat = renderer.material;
+    }
+
+    //render red color
+    public void RedColor()
+    {
+        renderer.material = BodyColorMat[1];
+        CurrMat = renderer.material;
+    }
+
+    //render green color
+    public void GreenColor()
+    {
+        renderer.material = BodyColorMat[2];
+        CurrMat = renderer.material;
+    }
+
+}
